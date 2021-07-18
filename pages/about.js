1340c6a9c22df72219f6bc2eb02d19f05aef90c1
Anders Strömberg <coltlaboy@yahoo.se>
2021-07-18 08:48:52 +0200

    add styles and layout

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur nihil corrupti adipisci, ullam similique dolore aliquid ipsa atque ipsam quasi provident quas et doloremque expedita nulla aut corporis suscipit consectetur.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur nihil corrupti adipisci, ullam similique dolore aliquid ipsa atque ipsam quasi provident quas et doloremque expedita nulla aut corporis suscipit consectetur.</p>
+    </div>
+  )
+}
+
+export default About
